60e9b6489797261458e3f74716ebb607b154c62a
wesley guerra <guerra_ssa@hotmail.com>
2020-11-07 16:10:19 -0300

    separating routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,11 @@
 import express, { request, response } from 'express';
+import routes from './routes/routes';
 
 const app = express();
-app.use(express.json())
 
+app.use(express.json());
+app.use(routes); // arquivo de rotas
 
-const users = [
-    'wesley',
-    'Vanildon',
-    'ana',
-]
-
-app.get('/users', (request, response) => {
-    const search = request.query.search;
-
-    const filteredUsers = search ? users.filter(user => user.includes(search as any)) : users
-
-    return response.json(filteredUsers)
-
-});
-
-app.get('/users/:id', (request, response) => {
-    const { id } = request.params;
-
-    const user = users[id as any]
-
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(user);
-});
 
 app.listen(3333, () => {
     console.log('run port 3333');
