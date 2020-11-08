1bcef6c9c81238ba863c90a39917662264dad524
wesley guerra <guerra_ssa@hotmail.com>
2020-11-08 18:00:04 -0300

    learning about props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Header title="aprendendo propriedade" />
+    </>
   );
 }
 
